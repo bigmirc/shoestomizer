e838a6c1c1ec677a3067cb9f5810c69e0469ca86
Alex G. Mircean <amircean@yahoo.com>
2020-05-29 19:52:04 +0300

    doar customize mai e de facut

--- routes/sneakers.js
+++ routes/sneakers.js
@@ -17,7 +17,7 @@ const storage = multer.diskStorage({
 // Init Upload
 const upload = multer({
   storage: storage,
-  limits:{fileSize: 1000000},
+  limits:{fileSize: 10000000},
   fileFilter: function(req, file, cb){
     checkFileType(file, cb);
   }
@@ -61,15 +61,12 @@ router.get('/sneakers', checkAuthenticated, async (req,res) => {
     }
 })
 
-
 //new sneaker route
 router.get('/new', checkAuthenticated, (req,res) => {
     res.render('new.ejs', {name:req.user.name, sneaker: new Sneaker()})
 })
 
-
-
-
+//remove image if sneaker's not created properly
 function removeSneakerImage(fileName){
   fs.unlink(path.join('./public/uploads/',fileName), err =>{
     if (err) console.log(err)
@@ -79,37 +76,99 @@ function removeSneakerImage(fileName){
 //create sneaker route
 router.post('/sneakers', upload.single("myImage"), async (req,res) => {
 
+  if (req.file == null){
+    fileName = ''
+  } else {
+    fileName = req.file.filename
+  }
 
-  console.log('serus')
+  const sneaker = new Sneaker({
+      name: req.body.name,
+      price: req.body.price,
+      imageName: fileName
+
+  })
+
+  try {
+    const newSneaker = await sneaker.save()
+    console.log(sneaker)
+    res.redirect('/sneakers')
+
+  } catch {
+
+    if (sneaker.imageName != ''){removeSneakerImage(sneaker.imageName)}
+    res.render('new.ejs', {name:req.user.name, sneaker: sneaker, errorMessage: 'Error creating sneaker'})
+
+  }
+})
+
+//route for showing one sneaker by id
+router.get('/sneakers/:id',  (req,res) =>{
+  res.send('show sneaker' + req.params.id)
+})
+
+//route for edit one sneaker by id
+router.get('/sneakers/:id/edit', async (req,res) =>{
+  try {
+    const sneaker = await Sneaker.findById(req.params.id)
+    res.render('edit.ejs', {name:req.user.name, sneaker: sneaker})
+  } catch (error) {
+    console.log(error)
+    res.redirect('/sneakers')
+    
+  }
+})
+
+//route for update one sneaker by id
+router.put('/sneakers/:id', upload.single("myImage"), async (req,res) =>{
+
+  let sneaker
+
+  try {
+    sneaker = await Sneaker.findById(req.params.id)
+    sneaker.name = req.body.name
+    sneaker.price = req.body.price
 
 
-    if (req.file == null){
-      fileName = ''
-    } else {
+    if (req.file != null){
       fileName = req.file.filename
+      sneaker.imageName= fileName
     }
+    
+    
+    await sneaker.save()
 
-    const sneaker = new Sneaker({
-        name: req.body.name,
-        price: req.body.price,
-        imageName: fileName
+    console.log(sneaker)
+    res.redirect('/sneakers')
 
-    })
+  } catch {
+
+    if (sneaker.imageName != null){removeSneakerImage(sneaker.imageName)}
+    res.render('edit.ejs', {name:req.user.name, sneaker: sneaker, errorMessage: 'Error updating sneaker'})
+
+  }
+
+})
+
+//route for del one sneaker by id
+router.delete('/sneakers/:id', async (req,res) =>{
+  let sneaker
+
+  try {
+    sneaker = await Sneaker.findById(req.params.id)
+    
+    await sneaker.remove()
+    removeSneakerImage(sneaker.imageName)
 
 
+    console.log(sneaker)
+    res.redirect('/sneakers')
 
+  } catch {
 
-    try {
-        const newSneaker = await sneaker.save()
-        console.log(sneaker)
-        res.redirect('/sneakers')
+    res.render('edit.ejs', {name:req.user.name, sneaker: sneaker, errorMessage: 'Error deleting sneaker'})
 
-    } catch {
-
-      if (sneaker.imageName != null){removeSneakerImage(sneaker.imageName)}
-      res.render('new.ejs', {name:req.user.name, sneaker: sneaker, errorMessage: 'Error creating sneaker'})
-
-    }
+  }
 })
 
 module.exports = router
